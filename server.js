70404098809cc17d4e494c5de7f0cd6b9cbe4a07
Vipin Gaba <vipin.gaba@sap.com>
2019-02-12 14:36:31 -0800

    porrt issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const hbs = require('hbs');
 
 const fs = require('fs');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
  var app =  express();
 
  hbs.registerPartials(__dirname + '/views/partials');
